b9ffdecd48564dad63c5b84120c65732a2d3d438
Jovanka <samudrajovanka20@gmail.com>
2020-11-30 23:29:30 +0700

    create bottom navigation bar

--- app/src/main/java/com/ksmandroid/taskreminder/MainActivity.kt
+++ app/src/main/java/com/ksmandroid/taskreminder/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.ksmandroid.taskreminder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.ksmandroid.taskreminder.R.layout.activity_main)
+
+        loadFragment(HomeFragment())
+        bnv_main.setOnNavigationItemSelectedListener(this)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(com.ksmandroid.taskreminder.R.id.fl_fragment_main, fragment)
+                .commit()
+            return true
+        }
+        return false
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            com.ksmandroid.taskreminder.R.id.home -> loadFragment(HomeFragment())
+            com.ksmandroid.taskreminder.R.id.profile -> loadFragment(ProfileFragment())
+            com.ksmandroid.taskreminder.R.id.add_task -> loadFragment(AddTaskFragment())
+        }
+        return true
     }
 }
